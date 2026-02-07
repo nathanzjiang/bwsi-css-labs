4d1ddd8a55bb4e4c7becc0a82365afcb950fb454
Nathan Jiang <nathanj0601@gmail.com>
2026-02-07 14:44:20 -0800

    Added personal into to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Nathan Jiang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone, I'm Nathan, a sophomore from SoCal. I really like photography. I look forward to learning with all of you!")
 
 if __name__ == "__main__":
     main()
